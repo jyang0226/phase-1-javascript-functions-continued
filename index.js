a8279bf5cdc845ea6ed49bb56b1302c1033bd0a9
Jaden Falbee <j.falbee04@gmail.com>
2023-06-27 20:29:48 -0400

    finishes lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(event = 'roller-skate') {
+    return `This Saturday, I want to ${event}!`
+}
+
+function mondayWork(event2 = 'go to the office') {
+    return `This Monday, I will ${event2}.`
+}
+
+function wrapAdjective(special) {
+    return function(whatYouAre) {
+        return `You are ${special}${whatYouAre}${special}!`
+    }
+}
